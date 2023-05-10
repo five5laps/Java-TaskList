1cf1c453e2ebe8d9e849cf62b6f8daeb1db13846
five5laps <68498669+five5laps@users.noreply.github.com>
2023-05-11 02:12:59 +0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.tashklistfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.tasklistjfx to javafx.fxml;
-    exports com.example.tasklistjfx;
-}
